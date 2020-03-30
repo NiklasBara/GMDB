a7e63b6241f241bc954dc7ff06fa204b1601e4b2
Mathias <mathias.kramer@gmail.com>
2020-03-30 14:43:52 +0200

    First Test written

--- gmdb-backend/src/test/java/de/owpgmdb/gmdbbackend/controller/MovieControllerTests.java
+++ gmdb-backend/src/test/java/de/owpgmdb/gmdbbackend/controller/MovieControllerTests.java
@@ -1,13 +1,19 @@
 package de.owpgmdb.gmdbbackend.controller;
 
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
 import de.owpgmdb.gmdbbackend.controllers.MovieController;
-
+import de.owpgmdb.gmdbbackend.repositories.MovieRepository;
 /**
  * MovieControllerTests
  */
@@ -18,11 +24,44 @@ public class MovieControllerTests {
     @MockBean
     MovieRepository movieRepository;
 
+    @Autowired
+	private MockMvc mvc;
 
 
+    //      private Long id;
+    //      private String title;
+    //      private Long releaseYear;
+    //      private List<Rating> ratings;
+    //      private List<Review> reviews;
+    private String jsonReturn = "
+    [
+        {
+            { \"id\": 1},
+            { \"title\": \"Alice im Wunderland\"},
+            { \"releaseYear\": 2020},
+        },
+        {
+            { \"id\": 2},
+            { \"title\": \"Alice im Wunderland2\"},
+            { \"releaseYear\": 2022},
+        }
+        ]
+    ";    
+        
     @Test
-    void canGetAllMoviesFromDataBase() {
+    void canGetAllMoviesFromDataBase() throws Exception {""
 
+      
+       // when(listMock.add(anyString())).thenReturn(false);
+ 
+        
+        given(this.movieRepository.findAll()).willReturn(jsonReturn);
+        
+        mvc.perform(get("/api/movies"))
+            .andExpect(status().isOk())
+			.andExpect(jsonPath("$.movies").isArray())
+            .andExpect(jsonPath("$.movies", hasSize(2)))
+            .andExpect(jsonPath("$.movies[0].id", is(1)));
     }
     
 }
