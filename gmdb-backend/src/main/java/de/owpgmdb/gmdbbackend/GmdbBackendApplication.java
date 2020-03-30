59f634a158a1cdb658a7293fdfd9396d2e6f5af6
SomeRandomDev747 <Rochan747@gmail.com>
2020-03-30 10:10:35 +0200

    init spring boot

---
+++ gmdb-backend/src/main/java/de/owpgmdb/gmdbbackend/GmdbBackendApplication.java
@@ -0,0 +1,13 @@
+package de.owpgmdb.gmdbbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmdbBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmdbBackendApplication.class, args);
+	}
+
+}
