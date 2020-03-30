4d1abaf90c416b251fd88373945ec76401b70587
Alois Nieder <alois@Aloiss-MBP.fritz.box>
2020-03-30 13:43:36 +0200

    added members to entities

--- gmdb-backend/src/main/java/de/owpgmdb/gmdbbackend/models/Review.java
+++ gmdb-backend/src/main/java/de/owpgmdb/gmdbbackend/models/Review.java
@@ -1,9 +1,25 @@
 package de.owpgmdb.gmdbbackend.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * Review
  */
+@NoArgsConstructor
+@Data
+@Entity
 public class Review {
 
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String text;
+
 }
